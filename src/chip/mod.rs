80a4d71d0f32e4b0177277c07346ae76b48d315c
Braden Dubois <braden.dubois@usask.ca>
2021-05-22 17:12:04 -0600

    add memory copying of program

--- src/chip/mod.rs
+++ src/chip/mod.rs
@@ -2,15 +2,23 @@ mod cpu;
 
 pub struct Chip {
     cpu: cpu::CPU,
-    memory: [u8; 4096]
+    memory: [u8; 4096],
+    stack: Vec<u16>
 }
 
 impl Chip {
 
-    pub fn new() -> Chip {
-        Chip {
+    pub fn new(data: Vec<u8>) -> Chip {
+        let mut chip = Chip {
             cpu: cpu::CPU::new(),
-            memory: [0; 4096]
+            memory: [0; 4096],
+            stack: Vec::new()
+        };
+
+        for (i, x) in data.iter().enumerate() {
+            chip.memory[i + 200] = *x;
         }
+
+        chip
     }
 }
